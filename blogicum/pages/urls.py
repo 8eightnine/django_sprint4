38627d362f27d1d55a9c1bacc51c23e30dd70b4a
Luthor Garret <luthorga@student.21-school.ru>
2025-02-20 23:36:12 +0700

    final upload

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -6,6 +6,4 @@ app_name = 'pages'
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name='pages/about.html'),
          name='about'),
-    path('rules/', TemplateView.as_view(template_name="pages/rules.html"),
-         name='rules'),
 ]
