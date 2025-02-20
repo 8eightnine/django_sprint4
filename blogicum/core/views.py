4c9fbd0cf9b0b7a98e20a7592dbac8f397e34653
Luthor Garret <luthorga@student.21-school.ru>
2025-02-20 22:13:21 +0700

    blog 4

--- blogicum/core/views.py
+++ blogicum/core/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
+
 def error_403(request, exception):
-    return render(request, 'pages/403.html', status=403)
+    return render(request, "pages/403.html", status=403)
 
 
 def error_404(request, exception):
-    return render(request, 'pages/404.html', status=404)
+    return render(request, "pages/404.html", status=404)
 
 
 def error_500(request):
-    return render(request, 'pages/500.html', status=500)
-
+    return render(request, "pages/500.html", status=500)
